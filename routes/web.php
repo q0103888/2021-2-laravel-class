793b518da4547b528174c51c46ffacb1a9beb3b8
q0103888 <72806455+q0103888@users.noreply.github.com>
2021-08-30 15:52:15 +0900

    간단한 laravel사용법

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/layout', function () {
+    return view('layouts.app');
+});
+
+Route::get('/hello', function () {
+    return view('hello');
+});
