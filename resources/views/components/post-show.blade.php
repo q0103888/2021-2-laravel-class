a503161a8157785dd2cc0a32c4fb6388e98942d9
q0103888 <72806455+q0103888@users.noreply.github.com>
2021-09-23 19:02:34 +0900

    0923

--- resources/views/components/post-show.blade.php
+++ resources/views/components/post-show.blade.php
@@ -7,7 +7,7 @@
             <span>첨부 이미지 없음</span>
         @endif
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">{{ $post->image }}</h5>
           <p class="card-text">
               {{ $post->content }}
           </p>
@@ -17,9 +17,29 @@
           <li class="list-group-item">수정일:{{ $post->updated_at }}</li>
           <li class="list-group-item">작성자:{{ $post->writer->name }}</li>
         </ul>
-        <div class="card-body">
-          <a href="#" class="card-link">수정하기</a>
-          <a href="#" class="card-link">삭제하기</a>
+        <div class="card-body flex">
+          <a href="{{ route('posts.edit', ['post'=>$post->id]) }}" class="card-link">수정하기</a>
+          <form id="form" class="ml-4" method="post" 
+                onsubmit="event.preventDefault(); confirmDelete(event)"
+                action="{{ route('posts.destroy', ['post'=>$post->id]) }}">
+            @csrf
+            @method('delete')
+            {{-- <input type="hidden" name="_method" value="delete"> --}}
+            <button onclick="confirmDelete()" type="submit">삭제하기</button>
+          </form>
         </div>
       </div>
+
+      <script>
+        function confirmDelete() {
+          myform = document.getElementById('form');
+          flag = confirm('정말 삭제할거야?');
+          if (flag) {
+            // 서브밋...
+            myform.submit();
+          }
+          //e.preventDefault(); //form이 서버로 전달되는 것을 막아준다.
+          
+        }
+      </script>
 </div>
