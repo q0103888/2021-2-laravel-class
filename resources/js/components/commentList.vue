e199f6c113752fbada3e8c922141ea83a6c7efe5
q0103888 <72806455+q0103888@users.noreply.github.com>
2021-10-14 20:17:01 +0900

    10-14 laravel class

---
+++ resources/js/components/commentList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button @click="getComments" class="btn btn-default">댓글 불러오기</button>
+        <comment-item v-for="(comment, index) in comments"
+            :key="index"/>
+        댓글 리스트
+    </div>
+</template>
+
+<script>
+import commentItem from './CommentItem.vue';
+export default {
+    props: ['post', 'loginuserId'],
+    data() {
+        return {
+            comments : [],
+        }
+    },
+
+    methods: {
+        getComments() {
+            this.comments=['1st comment', '2nd comment',
+            '3rd comment', '4th comment', '5th comment']
+        }
+    }
+}
+</script>
